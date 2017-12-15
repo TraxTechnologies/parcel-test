b62bf3fbd3d665fbc9b6d0ed076708bf6f9283f1
Alonzo Thomas <lenell16@gmail.com>
2017-12-15 11:03:17 -0700

    switch to rollup

--- dist/index.js
+++ dist/index.js
@@ -1,94 +1,17 @@
-// modules are defined as an array
-// [ module function, map of requires ]
-//
-// map of requires is short require name -> numeric require
-//
-// anything defined in a previous bundle is accessed via the
-// orig method which is the require for previous bundles
+'use strict';
 
-require = (function (modules, cache, entry) {
-  // Save the require from previous bundle to this closure if any
-  var previousRequire = typeof require === "function" && require;
+Object.defineProperty(exports, '__esModule', { value: true });
 
-  function newRequire(name, jumped) {
-    if (!cache[name]) {
-      if (!modules[name]) {
-        // if we cannot find the module within our internal map or
-        // cache jump to the current global require ie. the last bundle
-        // that was added to the page.
-        var currentRequire = typeof require === "function" && require;
-        if (!jumped && currentRequire) {
-          return currentRequire(name, true);
-        }
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-        // If there are other bundles on this page the require from the
-        // previous one is saved to 'previousRequire'. Repeat this as
-        // many times as there are bundles until the module is found or
-        // we exhaust the require chain.
-        if (previousRequire) {
-          return previousRequire(name, true);
-        }
+var React = _interopDefault(require('react'));
 
-        var err = new Error('Cannot find module \'' + name + '\'');
-        err.code = 'MODULE_NOT_FOUND';
-        throw err;
-      }
+var TestApp = function TestApp() {
+  return React.createElement(
+    'div',
+    null,
+    'This is a test'
+  );
+};
 
-      function localRequire(x) {
-        return newRequire(localRequire.resolve(x));
-      }
-
-      localRequire.resolve = function (x) {
-        return modules[name][1][x] || x;
-      };
-
-      var module = cache[name] = new newRequire.Module;
-      modules[name][0].call(module.exports, localRequire, module, module.exports);
-    }
-
-    return cache[name].exports;
-  }
-
-  function Module() {
-    this.bundle = newRequire;
-    this.exports = {};
-  }
-
-  newRequire.Module = Module;
-  newRequire.modules = modules;
-  newRequire.cache = cache;
-  newRequire.parent = previousRequire;
-
-  for (var i = 0; i < entry.length; i++) {
-    newRequire(entry[i]);
-  }
-
-  // Override the current require with this new one
-  return newRequire;
-})({11:[function(require,module,exports) {
-"use strict";function r(r){if(null===r||void 0===r)throw new TypeError("Object.assign cannot be called with null or undefined");return Object(r)}function t(){try{if(!Object.assign)return!1;var r=new String("abc");if(r[5]="de","5"===Object.getOwnPropertyNames(r)[0])return!1;for(var t={},e=0;e<10;e++)t["_"+String.fromCharCode(e)]=e;if("0123456789"!==Object.getOwnPropertyNames(t).map(function(r){return t[r]}).join(""))return!1;var n={};return"abcdefghijklmnopqrst".split("").forEach(function(r){n[r]=r}),"abcdefghijklmnopqrst"===Object.keys(Object.assign({},n)).join("")}catch(r){return!1}}var e=Object.getOwnPropertySymbols,n=Object.prototype.hasOwnProperty,o=Object.prototype.propertyIsEnumerable;module.exports=t()?Object.assign:function(t,c){for(var a,i,s=r(t),f=1;f<arguments.length;f++){a=Object(arguments[f]);for(var u in a)n.call(a,u)&&(s[u]=a[u]);if(e){i=e(a);for(var b=0;b<i.length;b++)o.call(a,i[b])&&(s[i[b]]=a[i[b]])}}return s};
-},{}],14:[function(require,module,exports) {
-"use strict";var e={};module.exports=e;
-},{}],16:[function(require,module,exports) {
-"use strict";function t(t){return function(){return t}}var n=function(){};n.thatReturns=t,n.thatReturnsFalse=t(!1),n.thatReturnsTrue=t(!0),n.thatReturnsNull=t(null),n.thatReturnsThis=function(){return this},n.thatReturnsArgument=function(t){return t},module.exports=n;
-},{}],9:[function(require,module,exports) {
-"use strict";function e(e){for(var t=arguments.length-1,n="Minified React error #"+e+"; visit http://facebook.github.io/react/docs/error-decoder.html?invariant="+e,r=0;r<t;r++)n+="&args[]="+encodeURIComponent(arguments[r+1]);throw t=Error(n+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings."),t.name="Invariant Violation",t.framesToPop=1,t}function t(e,t,n){this.props=e,this.context=t,this.refs=v,this.updater=n||x}function n(e,t,n){this.props=e,this.context=t,this.refs=v,this.updater=n||x}function r(){}function o(e,t,n){this.props=e,this.context=t,this.refs=v,this.updater=n||x}function u(e,t,n){var r,o={},u=null,l=null;if(null!=t)for(r in void 0!==t.ref&&(l=t.ref),void 0!==t.key&&(u=""+t.key),t)$.call(t,r)&&!A.hasOwnProperty(r)&&(o[r]=t[r]);var i=arguments.length-2;if(1===i)o.children=n;else if(1<i){for(var c=Array(i),f=0;f<i;f++)c[f]=arguments[f+2];o.children=c}if(e&&e.defaultProps)for(r in i=e.defaultProps)void 0===o[r]&&(o[r]=i[r]);return{$$typeof:k,type:e,key:u,ref:l,props:o,_owner:O.current}}function l(e){return"object"==typeof e&&null!==e&&e.$$typeof===k}function i(e){var t={"=":"=0",":":"=2"};return"$"+(""+e).replace(/[=:]/g,function(e){return t[e]})}function c(e,t,n,r){if(C.length){var o=C.pop();return o.result=e,o.keyPrefix=t,o.func=n,o.context=r,o.count=0,o}return{result:e,keyPrefix:t,func:n,context:r,count:0}}function f(e){e.result=null,e.keyPrefix=null,e.func=null,e.context=null,e.count=0,10>C.length&&C.push(e)}function a(t,n,r,o){var u=typeof t;"undefined"!==u&&"boolean"!==u||(t=null);var l=!1;if(null===t)l=!0;else switch(u){case"string":case"number":l=!0;break;case"object":switch(t.$$typeof){case k:case _:case g:case S:l=!0}}if(l)return r(o,t,""===n?"."+p(t,0):n),1;if(l=0,n=""===n?".":n+":",Array.isArray(t))for(var i=0;i<t.length;i++){var c=n+p(u=t[i],i);l+=a(u,c,r,o)}else if(null===t||void 0===t?c=null:(c=w&&t[w]||t["@@iterator"],c="function"==typeof c?c:null),"function"==typeof c)for(t=c.call(t),i=0;!(u=t.next()).done;)u=u.value,c=n+p(u,i++),l+=a(u,c,r,o);else"object"===u&&(r=""+t,e("31","[object Object]"===r?"object with keys {"+Object.keys(t).join(", ")+"}":r,""));return l}function p(e,t){return"object"==typeof e&&null!==e&&null!=e.key?i(e.key):t.toString(36)}function s(e,t){e.func.call(e.context,t,e.count++)}function y(e,t,n){var r=e.result,o=e.keyPrefix;e=e.func.call(e.context,t,e.count++),Array.isArray(e)?d(e,r,n,m.thatReturnsArgument):null!=e&&(l(e)&&(t=o+(!e.key||t&&t.key===e.key?"":(""+e.key).replace(E,"$&/")+"/")+n,e={$$typeof:k,type:e.type,key:t,ref:e.ref,props:e.props,_owner:e._owner}),r.push(e))}function d(e,t,n,r,o){var u="";null!=n&&(u=(""+n).replace(E,"$&/")+"/"),t=c(t,u,r,o),null==e||a(e,"",y,t),f(t)}var h=require("object-assign"),v=require("fbjs/lib/emptyObject"),m=require("fbjs/lib/emptyFunction"),b="function"==typeof Symbol&&Symbol.for,k=b?Symbol.for("react.element"):60103,_=b?Symbol.for("react.call"):60104,g=b?Symbol.for("react.return"):60105,S=b?Symbol.for("react.portal"):60106,j=b?Symbol.for("react.fragment"):60107,w="function"==typeof Symbol&&Symbol.iterator,x={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}};t.prototype.isReactComponent={},t.prototype.setState=function(t,n){"object"!=typeof t&&"function"!=typeof t&&null!=t&&e("85"),this.updater.enqueueSetState(this,t,n,"setState")},t.prototype.forceUpdate=function(e){this.updater.enqueueForceUpdate(this,e,"forceUpdate")},r.prototype=t.prototype;var P=n.prototype=new r;P.constructor=n,h(P,t.prototype),P.isPureReactComponent=!0;var R=o.prototype=new r;R.constructor=o,h(R,t.prototype),R.unstable_isAsyncReactComponent=!0,R.render=function(){return this.props.children};var O={current:null},$=Object.prototype.hasOwnProperty,A={key:!0,ref:!0,__self:!0,__source:!0},E=/\/+/g,C=[],q={Children:{map:function(e,t,n){if(null==e)return e;var r=[];return d(e,r,null,t,n),r},forEach:function(e,t,n){if(null==e)return e;t=c(null,null,t,n),null==e||a(e,"",s,t),f(t)},count:function(e){return null==e?0:a(e,"",m.thatReturnsNull,null)},toArray:function(e){var t=[];return d(e,t,null,m.thatReturnsArgument),t},only:function(t){return l(t)||e("143"),t}},Component:t,PureComponent:n,unstable_AsyncComponent:o,Fragment:j,createElement:u,cloneElement:function(e,t,n){var r=h({},e.props),o=e.key,u=e.ref,l=e._owner;if(null!=t){if(void 0!==t.ref&&(u=t.ref,l=O.current),void 0!==t.key&&(o=""+t.key),e.type&&e.type.defaultProps)var i=e.type.defaultProps;for(c in t)$.call(t,c)&&!A.hasOwnProperty(c)&&(r[c]=void 0===t[c]&&void 0!==i?i[c]:t[c])}var c=arguments.length-2;if(1===c)r.children=n;else if(1<c){i=Array(c);for(var f=0;f<c;f++)i[f]=arguments[f+2];r.children=i}return{$$typeof:k,type:e.type,key:o,ref:u,props:r,_owner:l}},createFactory:function(e){var t=u.bind(null,e);return t.type=e,t},isValidElement:l,version:"16.2.0",__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED:{ReactCurrentOwner:O,assign:h}},U=Object.freeze({default:q}),F=U&&q||U;module.exports=F.default?F.default:F;
-},{"object-assign":11,"fbjs/lib/emptyObject":14,"fbjs/lib/emptyFunction":16}],13:[function(require,module,exports) {
-"use strict";var _="SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED";module.exports=_;
-},{}],17:[function(require,module,exports) {
-"use strict";function e(e,r,i,o,t,a,f,s){if(n(r),!e){var u;if(void 0===r)u=new Error("Minified exception occurred; use the non-minified dev environment for the full error message and additional helpful warnings.");else{var d=[i,o,t,a,f,s],l=0;(u=new Error(r.replace(/%s/g,function(){return d[l++]}))).name="Invariant Violation"}throw u.framesToPop=1,u}}var n=function(e){};module.exports=e;
-},{}],15:[function(require,module,exports) {
-"use strict";var e,r=require("./emptyFunction"),t=r;module.exports=t;
-},{"./emptyFunction":16}],12:[function(require,module,exports) {
-"use strict";function t(t,e,o,r,s){}var e,o,r,s;module.exports=t;
-},{"./lib/ReactPropTypesSecret":13,"fbjs/lib/invariant":17,"fbjs/lib/warning":15}],10:[function(require,module,exports) {
-"use strict";
-},{"object-assign":11,"prop-types/checkPropTypes":12,"fbjs/lib/warning":15,"fbjs/lib/emptyFunction":16,"fbjs/lib/emptyObject":14,"fbjs/lib/invariant":17}],8:[function(require,module,exports) {
-"use strict";module.exports=require("./cjs/react.production.min.js");
-},{"./cjs/react.production.min.js":9,"./cjs/react.development.js":10}],2:[function(require,module,exports) {
-"use strict";function e(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0}),exports.TestApp=void 0;var t=require("react"),r=e(t),s=exports.TestApp=function(){return r.default.createElement("div",null,"This is a test")};
-},{"react":8}],3:[function(require,module,exports) {
-
-},{}],1:[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("./components");Object.keys(e).forEach(function(r){"default"!==r&&"__esModule"!==r&&Object.defineProperty(exports,r,{enumerable:!0,get:function(){return e[r]}})});var r=require("./containers");Object.keys(r).forEach(function(e){"default"!==e&&"__esModule"!==e&&Object.defineProperty(exports,e,{enumerable:!0,get:function(){return r[e]}})});var t=require("./pages");Object.keys(t).forEach(function(e){"default"!==e&&"__esModule"!==e&&Object.defineProperty(exports,e,{enumerable:!0,get:function(){return t[e]}})});var n=require("./redux");Object.keys(n).forEach(function(e){"default"!==e&&"__esModule"!==e&&Object.defineProperty(exports,e,{enumerable:!0,get:function(){return n[e]}})});var u=require("./lib");Object.keys(u).forEach(function(e){"default"!==e&&"__esModule"!==e&&Object.defineProperty(exports,e,{enumerable:!0,get:function(){return u[e]}})});
-},{"./components":2,"./containers":3,"./pages":3,"./lib":3,"./redux":3}]},{},[1])
+exports.TestApp = TestApp;
